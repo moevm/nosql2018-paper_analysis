0dec350a76367022526598f1c081df0a3c942136
awethon <gorodgorod63@gmail.com>
2018-10-24 22:12:31 +0300

    upd

--- src/main/scala/services/GraphFillService.scala
+++ src/main/scala/services/GraphFillService.scala
@@ -2,6 +2,7 @@ package services
 
 import com.twitter.util.Future
 import dao.PaperGraphDao
+import services.GraphFillService.CyberleninkaPageData
 
 class GraphFillService(dao: PaperGraphDao) {
   def createAuthor(name: String): Future[Unit] = {
@@ -15,4 +16,27 @@ class GraphFillService(dao: PaperGraphDao) {
   def createWroteRelation(authorName: String, paperTitle: String): Future[Unit] = {
     dao.createWroteRelation(authorName, paperTitle)
   }
+
+  def persistParsedPaper(data: CyberleninkaPageData): Future[Unit] = {
+    Future.join(
+      Seq(
+        createAuthor(data.authorName),
+        createPaper(data.paperTitle)
+      )
+    ).before(
+      createWroteRelation(data.authorName, data.paperTitle)
+    )
+  }
+}
+
+object GraphFillService {
+  case class CyberleninkaPageData(authorName: String,
+                                  paperTitle: String,
+                                  field: String = null,
+                                  tags: Seq[String] = null,
+                                  citations: Seq[Citation] = null,
+                                  journal: String = null)
+
+  case class Citation(author: String, title: String)
+
 }
